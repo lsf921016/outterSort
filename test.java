2659dcba4abc888c66c0aa4d40de131bc37f5326
lsf <lsf921016@126.com>
2017-03-21 14:32:21 -0400

    change name

--- test.java
+++ test.java
@@ -5,7 +5,7 @@ import java.io.File;
 /**
  * Created by lenovo on 2017/3/19.
  */
-public class testProgram {
+public class test {
     public static void main(String[] args) throws Exception {
         long start=System.currentTimeMillis();
         File inputFile=new File("E:\\javaStudy\\src\\outterSort\\inputFile.txt");
